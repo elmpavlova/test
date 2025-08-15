2955d74b3a4b1091d9575731663a88dc09b535b5
elmpavlova <elm.pavlova@gmail.com>
2025-08-15 16:32:30 +0900

    add db connection

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 print('Hello world')
+
+
+git 
+git status - проверка статуса
+git add app.py / <название файла> - добавление в index
+git status
+
+
+# git config --global user.name "Your name"
+# git config --global user.email "your.email@example.com"
+
+
+git config --global user.name "elmpavlova"
+git config --global user.email "elm.pavlova@gmail.com"
+
+git congig --list
+
+git commit -m "initial commit" /"комментарий к коммиту"
+
+git log - отобразить коммиты
